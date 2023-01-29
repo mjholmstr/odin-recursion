41a3d7559e293d081f98dfec3d368c1f4eafd0fd
Matt Holmstrom <mjholmstr@gmail.com>
2023-01-29 13:02:23 -0600

    Add recursive method as new function

--- fibonacci.js
+++ fibonacci.js
@@ -14,3 +14,23 @@ console.log(fibs(1));
 console.log(fibs(2));
 console.log(fibs(4));
 console.log(fibs(8));
+
+let fibsRec = (num) => {
+    if ( num <= 0 ) return "Please enter valid argument";
+    if (num === 1) {
+        return [0];
+    } else if ( num === 2 ) {
+        return [0, 1];
+    } else {
+        let resultArray = fibsRec(num - 1);
+        resultArray.push(resultArray[num - 2] + resultArray[num - 3]);
+        return resultArray
+    }
+
+}
+
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(2));
+console.log(fibsRec(4));
+console.log(fibsRec(8));
